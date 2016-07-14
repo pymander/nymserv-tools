8999f3d3f621fd0f4a208798bb04d1b1d2ec06ae
Erik L. Arneson <earneson@arnesonium.com>
2016-07-14 14:30:07 -0700

    Update the code. Include licensing.
    Just a few minor updates.

--- anondecode.pl
+++ anondecode.pl
@@ -1,9 +1,24 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
 #
 # Decode pseudonym messages.  We read the message in from STDIN, and
 # ARGV1 is the passphrase filename.
 #
-# $Id$
+# Copyright (c) 2002, Erik L. Arneson
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or (at
+#  your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+#  USA.
 
 use strict;
 use Term::ReadKey;
@@ -32,7 +47,7 @@ while (@phrases) {
     $pp = shift @phrases;
     $pt = $pgp->decrypt(Data => $msg,
                         Passphrase => $pp)
-      or die sprintf("Decryption error: %s\n%s\n",$pgp->errstr,$msg);
+      or die sprintf("Decryption error: %s\n%s\n", $pgp->errstr,$msg);
     ($msg = $pt) =~ s/\r\n/\n/g;
     $step++;
 }
--- buildblock.pl
+++ buildblock.pl
@@ -2,7 +2,7 @@
 #
 # Build a reply block for use with anonymous remailers.
 #
-# Copyright (c) 2002, Erik Arneson <erik@aarg.net>
+# Copyright (c) 2002, Erik L. Arneson
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -18,8 +18,6 @@
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
 #  USA.
-#
-# $Id$
 
 use strict;
 use Crypt::OpenPGP;
